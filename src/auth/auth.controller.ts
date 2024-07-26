6b665417aa217da5e197028478a9ddf3de8d2d2c
mac <mac@macs-MacBook-Air.local>
2024-07-26 07:15:31 +0100

    update auth controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, HttpStatus,UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, HttpStatus,UseGuards, Res } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from "express";
 
@@ -8,12 +8,16 @@ export class AuthController {
     @Get("/facebook")
   @UseGuards(AuthGuard("facebook"))
   async facebookLogin(): Promise<any> {
+    console.log('Initiating Facebook OAuth flow...');
     return HttpStatus.OK;
+
   }
 
   @Get("/facebook/redirect")
   @UseGuards(AuthGuard("facebook"))
-  async facebookLoginRedirect(@Req() req: Request): Promise<any> {
+  async facebookLoginRedirect(@Req() req: Request ,  @Res() res: Response): Promise<any> {
+    const user = req.user;
+    console.log('Facebook OAuth callback received:', user);
     return {
       statusCode: HttpStatus.OK,
       data: req.user,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/schemas/user.schema';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +11,15 @@ export class UsersController {
   @UseGuards(AuthGuard('facebook'))
   async getProfile(@Req() req) {
     return req.user;
+  }
+
+
+  @Post('createOrUpdate')
+  async createOrUpdate(@Body() userDto: any): Promise<User> {
+    try {
+      return await this.usersService.createOrUpdate(userDto);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
