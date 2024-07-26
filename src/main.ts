6ccdb4980b001eb6b54940666e1195b767494df3
mac <mac@macs-MacBook-Air.local>
2024-07-26 07:25:07 +0100

    added session middleware to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import {config} from "dotenv"
+import * as session from 'express-session';
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false }, 
+    }),
+  );
   const configBuilder = new DocumentBuilder()
     .setTitle(process.env.TITLE)
     .setDescription(process.env.DESCRIPTION)
